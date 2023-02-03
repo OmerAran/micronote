1c742f249c05527d5fbf54ceaaf281cbaeab46a6
OmerAran <omerr.aran@gmail.com>
2023-02-04 01:51:32 +0300

    note and user repositories added

---
+++ micronoteapp/src/main/java/com/omeraran/repository/NoteRepository.java
@@ -0,0 +1,9 @@
+package com.omeraran.repository;
+
+import com.omeraran.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+}
