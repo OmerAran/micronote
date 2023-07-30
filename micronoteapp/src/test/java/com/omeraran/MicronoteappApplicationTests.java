cf878849b7679b2e6ea9863e9c7870cc11a04190
omeraran <omeraranofficial@gmail.com>
2023-07-30 19:15:05 +0300

    dockerized application with a db.

---
+++ micronoteapp/src/test/java/com/omeraran/MicronoteappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omeraran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicronoteappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
